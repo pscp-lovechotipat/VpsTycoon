c7596414cb17d75183bd3f9fde420b09706db1b7
1REDfriend <83909061+1REDfriend@users.noreply.github.com>
2025-02-20 19:36:54 +0700

    Update GameState.java

--- src/main/java/com/vpstycoon/game/GameState.java
+++ src/main/java/com/vpstycoon/game/GameState.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class wwwGameState implements Serializable {
+public class GameState implements Serializable {
     private static final long serialVersionUID = 1L;
     
     private long money;
